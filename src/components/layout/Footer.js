d849e94f0390db2ecdc4657c56765e13db4ce6e9
Prashubh Atri <prashubh27@gmail.com>
2020-07-21 19:23:27 +0530

    Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,18 +10,22 @@ const Footer = () => {
                 </div>
             </div>
             <div className="row text-center">
-                <div className="col-12">
-                    Address
+                <div className="col-6 text-left pad_footer">
+                    <b>Durga Puri Branch, Shahdara:</b><br />
+                    B-2/1449, Durga Puri Chowk<br />
+                    Shahdara, Delhi-110093<br />
+                    <b>Laxmi Nagar Branch:</b><br />
+                    WA-119, First Floor<br/>
+                    Opposite Shakrpur School Block <br/>
+                    Delhi - 110092<br/>
+                </div>
+                <div className="col-6 text-right">
+                    Icons
                 </div>
             </div>
             <div className="row text-center">
                 <div className="col-12">
                     Quick Links
-                </div>
-            </div>
-            <div className="row text-center">
-                <div className="col-12">
-                    Icons
                 </div>
             </div>
             <div className="row text-center">

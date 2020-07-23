66e870e3090285c130ffc2248e47d8a7851714a1
Prashubh Atri <prashubh27@gmail.com>
2020-07-23 06:56:32 +0530

    test

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -3,7 +3,38 @@ import React from "react";
 const Landing = () => {
     return (
         <div>
-
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6 LandingCoursesCC"/>
+                    <div className="col-md-6">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam cum eius porro provident repudiandae. Dicta id nisi quo repellat sit. Doloribus nam neque numquam perspiciatis repellat, veritatis! Maxime, suscipit!</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam cum eius porro provident repudiandae. Dicta id nisi quo repellat sit. Doloribus nam neque numquam perspiciatis repellat, veritatis! Maxime, suscipit!</h2>
+                    </div>
+                    <div className="col-md-6 LandingCoursesAccounting"/>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 LandingCoursesFashionDesign"/>
+                    <div className="col-md-6">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam cum eius porro provident repudiandae. Dicta id nisi quo repellat sit. Doloribus nam neque numquam perspiciatis repellat, veritatis! Maxime, suscipit!</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam cum eius porro provident repudiandae. Dicta id nisi quo repellat sit. Doloribus nam neque numquam perspiciatis repellat, veritatis! Maxime, suscipit!</h2>
+                    </div>
+                    <div className="col-md-6 LandingCoursesCC"/>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 LandingCoursesCC"/>
+                    <div className="col-md-6">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam cum eius porro provident repudiandae. Dicta id nisi quo repellat sit. Doloribus nam neque numquam perspiciatis repellat, veritatis! Maxime, suscipit!</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

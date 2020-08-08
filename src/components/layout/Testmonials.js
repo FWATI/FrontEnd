4f5cf4afe5f31084b2bbe69200462b9aad958920
Prashubh Atri <prashubh27@gmail.com>
2020-08-08 12:56:56 +0530

    testimoials

--- src/components/layout/Testmonials.js
+++ src/components/layout/Testmonials.js
@@ -20,101 +20,184 @@ const Testimonials = () => {
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={SHIVANI} alt="Shivani" className="avatar-testimonials"/>
                             </div>
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6">
+                                It's a very good institute. I have done a Diploma course in Corporate Accounting.
+                                The Faculty is very good and the best part is they train us on live projects.
+                                <div className="text-right">
+                                    - Shivani
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6 hide-lg">
+                                I have done a Diploma course in Corporate Accounting.
+                                The Staff is very good and the fees is very reasonable.
+                                They provide placements also.
+                                <div className="text-right">
+                                    - Mridula
+                                </div>
                             </div>
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={MRIDULA} alt="Mridula" className="avatar-testimonials"/>
                             </div>
+                            <div className="col-lg-6 show-lg">
+                                I have done a Diploma course in Corporate Accounting.
+                                The Staff is very good and the fees is very reasonable.
+                                They provide placements also.
+                                <div className="text-right">
+                                    - Mridula
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={FARHEEN} alt="Farheen" className="avatar-testimonials"/>
                             </div>
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6">
+                                I have done a Diploma course in Fashion Designing.
+                                It's a very good institute and the teachers are very
+                                knowledgeable.
+                                <div className="text-right">
+                                    - Farheen
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6 hide-lg">
+                                I have dona a Diploma Course in Office Management.
+                                Teachers are very good and co-operative.
+                                <div className="text-right">
+                                    - Gaurav
+                                </div>
                             </div>
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={GAURAV} alt="Gaurav" className="avatar-testimonials"/>
                             </div>
+                            <div className="col-lg-6 show-lg">
+                                I have dona a Diploma Course in Office Management.
+                                Teachers are very good and co-operative.
+                                <div className="text-right">
+                                    - Gaurav
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={MANISH} alt="Manish" className="avatar-testimonials"/>
                             </div>
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6">
+                                I have dona a Diploma Course in Office Management.
+                                Placements are the best part as they have got a dedicated
+                                placement cell.
+                                <div className="text-right">
+                                    - Manish
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6 hide-lg">
+                                I have done a Diploma course in Beauty Culture.
+                                Faculty is very good and they taught us from the very basics to the advance level of
+                                Make up.
+                                <div className="text-right">
+                                    - Nishita
+                                </div>
                             </div>
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={NISHITA} alt="Nishita" className="avatar-testimonials"/>
                             </div>
+                            <div className="col-lg-6 show-lg">
+                                I have done a Diploma course in Beauty Culture.
+                                Faculty is very good and they taught us from the very basics to the advance level of
+                                Make up.
+                                <div className="text-right">
+                                    - Nishita
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={PREETI} alt="Preeti" className="avatar-testimonials"/>
                             </div>
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6">
+                                I have done a Diploma course in Tally and GST.
+                                they have got experrienced faculty and the best part is they help us to work on Live Projects.
+                                <div className="text-right">
+                                    - Preeti
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6 hide-lg">
+                                I have done a Diploma course in Office Management.
+                                I would recommend this institute to everyone because it is very good.
+                                <div className="text-right">
+                                    - Kapil
+                                </div>
                             </div>
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={KAPIL} alt="Kapil" className="avatar-testimonials"/>
                             </div>
+                            <div className="col-lg-6 show-lg">
+                                I have done a Diploma course in Office Management.
+                                I would recommend this institute to everyone because it is very good.
+                                <div className="text-right">
+                                    - Kapil
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={ANITA} alt="Anita" className="avatar-testimonials"/>
                             </div>
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6">
+                                I have done a Diploma course in Fashion Designing.
+                                The Teachers are very good and Fees is very genuine.
+                                <div className="text-right">
+                                    - Anita
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
-                            <div className="col-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
+                            <div className="col-lg-6 hide-lg">
+                                I have done a Diploma course in Spoken English.
+                                Its a very good institute.
+                                After this course, I can speak english very fluently.
+                                <div className="text-right">
+                                    - Nisha
+                                </div>
                             </div>
-                            <div className="col-4">
+                            <div className="col-lg-4">
                                 <img src={NISHA} alt="Nisha" className="avatar-testimonials"/>
+                            </div>
+                            <div className="col-lg-6 show-lg">
+                                I have done a Diploma course in Spoken English.
+                                Its a very good institute.
+                                After this course, I can speak english very fluently.
+                                <div className="text-right">
+                                    - Nisha
+                                </div>
                             </div>
                         </div>
                     </div>

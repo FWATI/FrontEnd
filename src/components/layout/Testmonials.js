7165cb9d6739c08a4b12acb45d6821b7ac3a8a7b
Prashubh Atri <prashubh27@gmail.com>
2020-08-08 12:15:02 +0530

    testimoials pics

--- src/components/layout/Testmonials.js
+++ src/components/layout/Testmonials.js
@@ -1,14 +1,27 @@
 import React from "react";
+import KAPIL from "../../resources/images/testimonials/Kapil.jpeg";
+import ANITA from "../../resources/images/testimonials/Anita.jpeg";
+import FARHEEN from "../../resources/images/testimonials/Farheen.jpeg";
+import GAURAV from "../../resources/images/testimonials/Gaurav.jpeg";
+import MANISH from "../../resources/images/testimonials/Manish.jpeg";
+import NISHITA from "../../resources/images/testimonials/Nishita.jpeg";
+import PREETI from "../../resources/images/testimonials/Preeti.jpeg";
+import SHIVANI from "../../resources/images/testimonials/Shivani.jpeg"
+import MRIDULA from "../../resources/images/testimonials/Mridula.jpeg"
+import NISHA from "../../resources/images/testimonials/NIsha.jpeg"
 
 const Testimonials = () => {
     return (
         <div className="container-fluid  bg-img-testimonials my_container">
             <div className="container">
                 <div className="row">
+                    <div className="col-lg-12 text-center text-light heading-testimonials">
+                        Testimonials
+                    </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
                             <div className="col-4">
-                                IMAGE
+                                <img src={SHIVANI} alt="Shivani" className="avatar-testimonials"/>
                             </div>
                             <div className="col-6">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
@@ -21,14 +34,14 @@ const Testimonials = () => {
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
                             </div>
                             <div className="col-4">
-                                IMAGE
+                                <img src={MRIDULA} alt="Mridula" className="avatar-testimonials"/>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
                             <div className="col-4">
-                                IMAGE
+                                <img src={FARHEEN} alt="Farheen" className="avatar-testimonials"/>
                             </div>
                             <div className="col-6">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
@@ -41,14 +54,14 @@ const Testimonials = () => {
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
                             </div>
                             <div className="col-4">
-                                IMAGE
+                                <img src={GAURAV} alt="Gaurav" className="avatar-testimonials"/>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
                             <div className="col-4">
-                                IMAGE
+                                <img src={MANISH} alt="Manish" className="avatar-testimonials"/>
                             </div>
                             <div className="col-6">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
@@ -61,14 +74,14 @@ const Testimonials = () => {
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
                             </div>
                             <div className="col-4">
-                                IMAGE
+                                <img src={NISHITA} alt="Nishita" className="avatar-testimonials"/>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
                             <div className="col-4">
-                                IMAGE
+                                <img src={PREETI} alt="Preeti" className="avatar-testimonials"/>
                             </div>
                             <div className="col-6">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
@@ -81,14 +94,14 @@ const Testimonials = () => {
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
                             </div>
                             <div className="col-4">
-                                IMAGE
+                                <img src={KAPIL} alt="Kapil" className="avatar-testimonials"/>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
                         <div className="row my_pad text-light  my_text m-2 shadow">
                             <div className="col-4">
-                                IMAGE
+                                <img src={ANITA} alt="Anita" className="avatar-testimonials"/>
                             </div>
                             <div className="col-6">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
@@ -101,7 +114,7 @@ const Testimonials = () => {
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam cupiditate dolore doloribus eum neque nostrum perspiciatis tempora? Labore nemo nostrum repellat sapiente tempore voluptatem! Commodi consectetur eum fuga mollitia.
                             </div>
                             <div className="col-4">
-                                IMAGE
+                                <img src={NISHA} alt="Nisha" className="avatar-testimonials"/>
                             </div>
                         </div>
                     </div>

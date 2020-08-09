92bf559204bfc604348d577a136a2839e0c3685b
Prashubh Atri <prashubh27@gmail.com>
2020-08-09 12:51:22 +0530

    forms

--- src/components/layout/Franchise.js
+++ src/components/layout/Franchise.js
@@ -9,48 +9,50 @@ const Franchise = () => {
                         Advantages
                     </div>
                     <div className="col-xl-6 box-from">
-                        <form name="franchise" method="POST" data-netlify="true">
-                            <p className="form-group">
+                        <form name="franchise" method="POST" netlify>
+                            <div className="form-group">
                                 <input type="text" name="name" className="form-control" placeholder="Applicant Name: "/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="text" name="fatherName" className="form-control" placeholder="Father/Husband Name: "/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <select className="form-control" id="exampleFormControlSelect1" name="gender">
                                     <option>--Select Gender--</option>
                                     <option>Male</option>
                                     <option>Female</option>
                                 </select>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="date" name="dob" placeholder="DOB" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="email" name="email" placeholder="Email" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="tel" name="contactNumber" placeholder="Contact Number" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="tel" name="altContactNumber" placeholder="Alternative Contact Number" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="text" name="qualification" placeholder="Highest Qualification" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="text" name="centreName" placeholder="Centre Name" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="text" name="proposedCentreName" placeholder="Proposed Centre Name" className="form-control"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="text" name="address" className="form-control" placeholder="Address"/>
-                            </p>
-                            <p className="form-group">
+                            </div>
+                            <div className="form-group">
                                 <input type="text" name="info" className="form-control" placeholder="Any other Info"/>
-                            </p>
-                            <button type="submit" className="btn btn-success form-control">Submit</button>
+                            </div>
+                            <div className="text-center">
+                                <button type="submit" className="btn btn-success">Submit</button>
+                            </div>
                         </form>
                     </div>
                 </div>

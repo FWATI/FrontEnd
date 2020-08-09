650835b9a02815b969d8076a935dcb9e9985b086
Prashubh Atri <prashubh27@gmail.com>
2020-08-09 13:23:19 +0530

    trial

--- src/components/layout/Franchise.js
+++ src/components/layout/Franchise.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ContactForm from "./Form";
 
 const Franchise = () => {
     return (
         <div className="grad text-light">
             <div className="container-fluid p-5">
                 <div className="row">
-                    <div className="col-xl-6 text-center p-5">
+                    <div className="col-xl-6 text-center p-4">
                         <div className="franchise-head text-dark-red">Business Opportunities:</div>
                         <div className="franchise-desc">
                             Anyone having a passion for education and training could partner with us as a franchise.
@@ -16,50 +17,10 @@ const Franchise = () => {
                             <a href="tel:9873731755" className="text-light text-cyan">+91-9873731755</a>.
                         </div>
                     </div>
-                    <div className="col-xl-4 box-from">
-                        <form method="POST" data-netlify="true">
-                            <div className="form-group">
-                                <input type="text" name="name" className="form-control" placeholder="Applicant Name: "/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" name="fatherName" className="form-control" placeholder="Father/Husband Name: "/>
-                            </div>
-                            <div className="form-group">
-                                <select className="form-control" id="exampleFormControlSelect1" name="gender">
-                                    <option>--Select Gender--</option>
-                                    <option>Male</option>
-                                    <option>Female</option>
-                                </select>
-                            </div>
-                            <div className="form-group">
-                                <input type="date" name="dob" placeholder="DOB" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="email" name="email" placeholder="Email" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="tel" name="contactNumber" placeholder="Contact Number" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="tel" name="altContactNumber" placeholder="Alternative Contact Number" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" name="qualification" placeholder="Highest Qualification" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" name="centreName" placeholder="Centre Name" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" name="proposedCentreName" placeholder="Proposed Centre Name" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" name="address" className="form-control" placeholder="Address"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" name="info" className="form-control" placeholder="Any other Info"/>
-                            </div>
-                            <button type="submit" className="btn btn-success form-control">Submit</button>
-                        </form>
+
+
+                    <div className="col-xl-6">
+                        <ContactForm/>
                     </div>
                 </div>
             </div>

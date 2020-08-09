f138a09411f9153c0e28e7b4355ddd5be281eedc
Prashubh Atri <prashubh27@gmail.com>
2020-08-09 13:09:36 +0530

    From desc

--- src/components/layout/Franchise.js
+++ src/components/layout/Franchise.js
@@ -3,13 +3,21 @@ import React from "react";
 const Franchise = () => {
     return (
         <div className="grad text-light">
-            <div className="container p-5">
+            <div className="container-fluid p-5">
                 <div className="row">
-                    <div className="col-xl-6">
-                        Advantages
+                    <div className="col-xl-6 text-center p-5">
+                        <div className="franchise-head text-dark-red">Business Opportunities:</div>
+                        <div className="franchise-desc">
+                            Anyone having a passion for education and training could partner with us as a franchise.
+                            As a Future Ace franchise you will be involved in more than simply running a learning center,
+                            you will have an active role in creating positive change in students and their families life.
+                            If you are interested by what we have achieved and would love to become a part of the growing network,
+                            Contact Us by filling out this form or Call on
+                            <a href="tel:9873731755" className="text-light text-cyan">+91-9873731755</a>.
+                        </div>
                     </div>
-                    <div className="col-xl-6 box-from">
-                        <form name="franchise" method="POST" netlify>
+                    <div className="col-xl-4 box-from">
+                        <form method="POST" data-netlify="true">
                             <div className="form-group">
                                 <input type="text" name="name" className="form-control" placeholder="Applicant Name: "/>
                             </div>
@@ -50,9 +58,7 @@ const Franchise = () => {
                             <div className="form-group">
                                 <input type="text" name="info" className="form-control" placeholder="Any other Info"/>
                             </div>
-                            <div className="text-center">
-                                <button type="submit" className="btn btn-success">Submit</button>
-                            </div>
+                            <button type="submit" className="btn btn-success form-control">Submit</button>
                         </form>
                     </div>
                 </div>
